8fc2055f363d5733f78f8a0b1570745c531dae7a
Adil Leghari <adilio@gmail.com>
2026-07-24 01:36:39 -0700

    Refine 4dl footer copy

--- src/components/BrandFooter.tsx
+++ src/components/BrandFooter.tsx
@@ -1,9 +1,9 @@
 export function BrandFooter() {
   return (
     <footer className="brand-footer">
-      <div><strong>Rhabbit</strong> · a 4dl app by Adil Leghari</div>
+      <div>A 4dl project by Adil Leghari</div>
       <div>
-        <a href="https://4dl.ca">More 4dl apps</a>
+        <a href="https://4dl.ca">Explore 4dl</a>
         {" · "}
         <a href="https://github.com/adilio/Rhabbit" target="_blank" rel="noreferrer">Source</a>
       </div>
